f5603188c3f2d3e6243b4b70765c0d047ac0a4fc
riber <871353782@qq.com>
2015-10-14 22:40:30 +0800

    111

--- managers/userManager.js
+++ managers/userManager.js
@@ -2,7 +2,7 @@ var models = require("../models");
 var User=models.User;
 
 module.exports.getUsers=function(params,err,callback){
-	User.findAll().then(function(users){
+	User.findAll({include: [models.Weibo]}).then(function(users){
          callback(users);
 	});	
 }
